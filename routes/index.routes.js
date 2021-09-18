561697c9837c41ab7d995ccf51e50679f199bb6d
June Oh <JuneOh710@gmail.com>
2021-09-18 16:16:39 -0400

    added creating new board routes.

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 const router = Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index.views.ejs', { title: 'bubl board' });
+/* GET index page. */
+router.get('/', (req, res, next) => {
+  res.render('index.views.ejs');
 });
+
+// make new board
+router.get('/board/new', (req, res, next) => {
+  res.render('create-board.views.ejs');
+})
+
+router.post('/board', (req, res, next) => {
+  const { title, duration } = req.body;
+  res.render('board.views.ejs', { title, duration });
+})
 
 export default router;
