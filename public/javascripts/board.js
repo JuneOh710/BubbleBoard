d924382099afe3fe442d8b0e51fbe1d761900d9a
June Oh <JuneOh710@gmail.com>
2021-09-19 02:21:11 -0400

    Fixed going out of border bug.

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,16 +1,9 @@
 function RandomizePosition() {
   buble = document.getElementById("buble");
-  let y = 31 * Math.random() + 16;
-  let x = 113 * Math.random();
+  let y = 6 * Math.random();
+  let x = 60 * Math.random();
   question.style.top = y + "rem";
   question.style.left = x + "rem";
-
-  //   buble = document.getElementById("buble");
-  //   buble.style.top = y + "rem";
-  //   buble.style.left = x + "rem";
-  //   console.log(x, y);
-  //   console.log(question.style.top, question.style.left);
-  //   console.log(buble.style.top, buble.style.left);
 }
 
 RandomizePosition();
